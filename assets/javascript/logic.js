18e645ee80acb897cf286dd9c064b8ca9f212987
Inomzhon Ibragimov <mail4inom@users.noreply.github.com>
2018-03-25 19:36:47 -0400

    pausing gif code added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
     var topics = ["cat", "dog", "putin", "trump", "skunk", "pig", "donkey", "snake", "star wars", "terminator", "bird", "usa", "russia", "jimmy", "50cent", "ufc", "mma", "coding", "hacker"];
     var still;
     var animate;
+
     function displayGiphy() {
-        $("#giphys-view").empty();
+        // $("#giphys-view").empty();
         var topics = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=8gTAnRtd6rOl1UFJ17AYYmzMBV2rK2fv&limit=12"
         $.ajax({
@@ -17,14 +19,34 @@ $(document).ready(function(){
                 var rating = result[i].rating;
 
                 var ratingDisplay = $("<p>").text("Rating " + rating);
-                var gifDisplay = $("<img >");
-                gifDisplay.attr("src", result[i].images.fixed_height.url);
+                var gifDisplay = $("<img>");
+                gifDisplay.addClass("animate");
+                gifDisplay.attr("data-state", "still");
+                gifDisplay.attr("data-still", result[i].images.fixed_height_still.url);
+                gifDisplay.attr("data-animate", result[i].images.fixed_height.url);
+                gifDisplay.attr("src", result[i].images.fixed_height_still.url);
 
                 gifs.append(ratingDisplay, gifDisplay);
                 $("#giphys-view").prepend(gifs);
             }
+            $(".animate").on("click", function () {
+
+                var state = $(this).attr("data-state");
+
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                    console.log(this);
+                }
+                if (state === "animate") {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                    console.log(this);
+                }
+            });
         });
     }
+
     function buttonMaker() {
         $("#buttons-view").empty();
         for (var i = 0; i < topics.length; i++) {
@@ -35,6 +57,7 @@ $(document).ready(function(){
             $("#buttons-view").append(btn);
         }
     }
+
     $("#add-giphy").on("click", function (event) {
         event.preventDefault();
         var gifs = $("#giphy-input").val().trim();
@@ -48,30 +71,6 @@ $(document).ready(function(){
     $(document).on("click", ".gif", displayGiphy)
     buttonMaker();
 
-    });
+});
 
 
-                ///////////////////////////////////////////////////
-// var still = $(this).attr(gifDisplayStill);
-// console.log(this);
-// var animate = $(this).attr(gifDisplay);
-            // function onClick() {
-            //     if (still === ) {
-            //         animate;
-            //     } else if ($(".img").val() === animate) {
-            //         still;
-            //     }
-            // }
-            /////////////////////////////////////
-            // var state = $(this).attr("data-state");
-//////////////////////////////////////////////
-// if(state === "still"){
-//         $(this).attr("src", $(this).attr("data-animate"));
-//         $(this).attr("data-state", "animate");
-//         console.log(this);
-// }
-// if(state === "animate"){
-//        $(this).attr("src", $(this).attr("data-still"));
-//         $(this).attr("data-state", "still");
-//         console.log(this);
-// }
